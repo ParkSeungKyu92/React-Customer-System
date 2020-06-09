832d0d73eda631247abad40001843ccbd6418f08
ParkSeungKyu <effortsk@naver.com>
2020-06-09 21:48:20 +0900

    add Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Customer from './Components/Customer'
+import Customer from './Components/Customer';
+import Paper from '@material-ui/core/Paper'; 
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import  {withStyles } from '@material-ui/core/styles';
 
+//css 꾸미기
+const styles = theme => ({
+  root : {
+    width : '100%',
+    marginTop : theme.spacing.uint * 3, //위쪽 여백 3의 가중치만큼
+    overflowX: "auto"
+  },
+  table: {
+    minWidth : 1080
+  }
+
+})
 const customer = [
 {
   id    : 1,
@@ -31,25 +50,29 @@ const customer = [
 
 class App extends Component{
   render(){
+    const {classes} = this.props;
     return (
-      <div>
-        {
-          //map사용시 key 설정해줘야한다!
-          customer.map(c => {
-            return (<Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birth={c.birth}
-              gender={c.gender}
-              job={c.job}>
-            </Customer>);
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customer.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birth={c.birth} gender={c.gender} job={c.job}></Customer>); })}
+            </TableBody>
+          </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
+
+//map사용시 key 설정해줘야한다!
